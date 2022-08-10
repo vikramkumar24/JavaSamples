807cc85eaf7a0fe2445814bd1371418fc5a86daf
vikra <vikra@DESKTOP-UOUIVNV>
2022-08-10 10:36:21 +0530

    list iterarion

--- src/main/java/com/lambdas/ListNull.java
+++ src/main/java/com/lambdas/ListNull.java
@@ -1,10 +1,22 @@
 package com.lambdas;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class ListNull {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub.
-
+		List<Integer> testList = Arrays.asList(123, 456, 789, 224, null);
+				testList.stream().filter(Objects::nonNull).map(num -> {
+			String output = "";
+			if(num % 2 == 0) {
+				output = num+"e,";
+			}else {
+				output = "o"+num+",";
+			}
+			return output;
+		}).forEach(System.out::print);
 	}
 
 }
